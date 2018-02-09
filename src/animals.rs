926b4ef31cbedd1c69833c896bdc9535c93b369e
Tosil E. Velkov <tosil.velkov@MUSALA.COM>
2018-02-09 15:37:33 +0200

    init commit

---
+++ src/animals.rs
@@ -0,0 +1,32 @@
+extern crate requests;
+use self::requests::ToJson;
+
+pub fn woof_image() -> Result<String, String> {
+    let response = match requests::get("https://random.dog/woof") {
+         Ok(res) => res,
+         Err(_) => return Err(String::from("Error getting a doggo right now :("))
+    };
+
+    if response.status_code() != requests::StatusCode::Ok {
+        return Err(String::from("Cannot get a doggo right now :("));
+    }
+
+    let data = response.text().unwrap();
+
+    let url = format!("https://random.dog/{}", data);
+    return Ok(url);
+}
+
+pub fn meow_image() -> Result<String, String> {
+    let response = match requests::get("https://random.cat/meow") {
+         Ok(res) => res,
+         Err(_) => return Err(String::from("Error getting a kitteh right now :("))
+    };
+
+    if response.status_code() != requests::StatusCode::Ok {
+        return Err(String::from("Cannot get a kitteh right now :("));
+    }
+    let data = response.json().unwrap();
+
+    return Ok(data["file"].to_string());
+}
---
+++ src/main.rs
@@ -0,0 +1,66 @@
+#[macro_use] extern crate serenity;
+extern crate serde_json;
+
+use serenity::client::Client;
+use serenity::prelude::EventHandler;
+use serenity::framework::standard::StandardFramework;
+use serde_json::{Value};
+use std::fs::File;
+use std::io::Read;
+//use std::env;
+
+mod animals;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
+fn main() {
+    let v: Value = load_settings();
+    start_discord(&v);
+}
+
+fn load_settings() -> Value
+{
+    let mut file = File::open("settings.json").unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+    return serde_json::from_str(&data).unwrap();
+}
+
+fn start_discord(settings: &Value) {
+    let token = settings["token"].as_str().unwrap();
+    let prefix = settings["prefix"].as_str().unwrap();
+    
+    // Login with a bot token from the environment
+    let mut client = Client::new(token, Handler)
+        .expect("Error creating client");
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix(prefix)) 
+        .cmd("dog", dog)
+        .cmd("cat", cat)
+        );
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+command!(dog(_context, message) {
+    let mut res = match animals::woof_image() {
+            Ok(img) => img,
+            Err(e) => e,
+        };
+
+    let _ = message.reply(&res);
+});
+
+command!(cat(_context, message) {
+    let mut res = match animals::meow_image() {
+            Ok(img) => img,
+            Err(e) => e,
+        };
+
+    let _ = message.reply(&res);
+});
